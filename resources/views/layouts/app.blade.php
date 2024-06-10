b909c8424a429749616b268ce69626d945b34392
SwiftMaxz <gpnaxs283@gmail.com>
2024-06-10 15:26:06 +0700

    backend dashboard user

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,8 +13,13 @@
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
     <link rel= "stylesheet"
         href= "https://maxst.icons8.com/vue-static/landings/line-awesome/font-awesome-line-awesome/css/all.min.css">
-    <!-- Scripts -->
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+<!-- Scripts -->
     @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
 </head>
 
 <body class="font-sans antialiased">
@@ -32,9 +37,10 @@
 
         <!-- Page Content -->
         <main>
-
             {{ $slot }}
         </main>
+
+        @include('layouts.footer')
     </div>
 </body>
 
---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,5 @@
+<footer class="footer mt-auto bg-dark text-white text-center py-3">
+    <div class="container">
+        <p class="mb-0">&copy; {{ date('Y') }} Rent a Car</p>
+    </div>
+</footer>        
