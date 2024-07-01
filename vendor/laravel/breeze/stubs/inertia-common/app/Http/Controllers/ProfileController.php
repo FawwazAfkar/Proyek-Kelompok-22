b82f11b17d54f5e8b53eeb945dcfe45fd5df79e6
MUHAMAD GALIH <100691045+muhgalihhh@users.noreply.github.com>
2024-07-01 11:29:13 +0700

    nambahin welcome page

--- vendor/laravel/breeze/stubs/inertia-common/app/Http/Controllers/ProfileController.php
+++ vendor/laravel/breeze/stubs/inertia-common/app/Http/Controllers/ProfileController.php
@@ -35,6 +35,8 @@ class ProfileController extends Controller
             $request->user()->email_verified_at = null;
         }
 
+
+
         $request->user()->save();
 
         return Redirect::route('profile.edit');
